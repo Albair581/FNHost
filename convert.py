029c96c8036c5d4cd88f347778dd775f701ae3c4
Albair581 <148535174+Albair581@users.noreply.github.com>
2026-01-26 10:47:54 +0800

    updated files

--- convert.py
+++ convert.py
@@ -7,19 +7,20 @@ sheet = wb.active
 lob = []
 fine = [{"org": sheet.title, "branches": []}]
 # Loop through rows
-for row in sheet.iter_rows(min_row=2, values_only=True, max_row=305, max_col=12):
+for row in sheet.iter_rows(min_row=2, values_only=True, max_row=305, max_col=13):
     data = {
         "name": row[0] + f"（{row[1]}）",
         "address": row[2],
-        "hours": row[7],
+        "hours": row[6],
         "phone": row[4],
         "fax": row[5],
-        "coordinates": row[3],
+        "coordinates": [float(row[3].split(",")[0]), float(row[3].split(",")[1])],
         "transportation": row[7],
         "parking": row[8],
         "accepted_items": row[9],
         "notes": row[10],
         "maps_url": row[11],
+        "update_time": str(row[12]),
     }
     lob.append(data)
 
